77b2c1920e74f1fcaf098498b52c92c9054be6f3
Carlos Santos <carlos.santos.dev@gmail.com>
2024-11-21 15:02:32 -0300

    feat(web): interception route for create organization

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -4,12 +4,19 @@ import { isAuthenticated } from '@/auth/auth'
 
 export default async function AppLayout({
   children,
+  teste,
 }: Readonly<{
   children: React.ReactNode
+  teste: React.ReactNode
 }>) {
   if (await !isAuthenticated()) {
     redirect('/auth/sign-in')
   }
 
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      {teste}
+    </>
+  )
 }
