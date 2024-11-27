08cd4c16974f8a490ab72e27c5d9a2bac4f44d50
Carlos Santos <carlos.santos.dev@gmail.com>
2024-11-27 09:51:15 -0300

    lint(api): file lint

--- apps/api/src/http/routes/auth/authenticate-with-github.ts
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -3,9 +3,8 @@ import type { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
-import { prisma } from '@/lib/prisma'
-
 import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
+import { prisma } from '@/lib/prisma'
 
 export async function authenticateWithGithub(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -48,7 +47,6 @@ export async function authenticateWithGithub(app: FastifyInstance) {
       })
 
       const githubAccessTokenData = await githubAccessTokenResponse.json()
-      console.log(githubAccessTokenData)
 
       const { access_token: githubAccessToken } = z
         .object({
@@ -65,7 +63,6 @@ export async function authenticateWithGithub(app: FastifyInstance) {
       })
 
       const githubUserData = await githubUserResponse.json()
-      console.log(githubUserData)
 
       const {
         id: githubId,
